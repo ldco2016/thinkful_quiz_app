f32fe1a67445624e14b94e18b2d6b8ee2f5bb325
danale <danale@danales-MacBook-Pro.local>
2018-01-07 10:45:24 -0500

    Added Validation to JS

--- js/callback.js
+++ js/callback.js
@@ -1,49 +1,28 @@
-// First way to do it
-var posts = [
-      { id: 23, title: 'Daily JS News' },
-      { id: 52, title: 'Code Refactor City' },
-      { id: 105, title: 'The Brightest Ruby' }
-  ];
+function submitAnswers() {
+  var total = 5;
+  var score = 0;
+  // Get User Input
+  var q1 = document.forms['quizForm']['q1'].value;
+  var q2 = document.forms['quizForm']['q2'].value;
+  var q3 = document.forms['quizForm']['q3'].value;
+  var q4 = document.forms['quizForm']['q4'].value;
+  var q5 = document.forms['quizForm']['q5'].value;
 
-function handlePosts() {
-  posts.forEach(function(post) {
-    savePost(post);
-  });
-}
+  // Validation
+  for (i = 1; i <= total; i++) {
+    if (eval('q'+i) == null || eval('q'+i) == '') {
+      alert('You missed question '+ i);
+      return false;
+    }
+  }
 
-function savePost(post){
-  console.log('saving post:' + post.title);
-}
+  // Set Correct Answers
+  var answers = ["d","b","c","a","b"];
 
-
-handlePosts(); // don't forget to call the function to se something in the console :)
-
-// Second way to do it.
-function handlePosts() {
-  var posts = [
-        { id: 23, title: 'Daily JS News' },
-        { id: 52, title: 'Code Refactor City' },
-        { id: 105, title: 'The Brightest Ruby' }
-    ];
-
-    posts.forEach(function(post) {
-      console.log(post);
-    });
-}
-
-// Third way to do it
-function handlePosts() {
-    var posts = [
-      { id: 23, title: 'Daily JS News' },
-      { id: 52, title: 'Code Refactor City' },
-      { id: 105, title: 'The Brightest Ruby' }
-    ];
-
-    posts.forEach(function(post) {
-      savePost(post);
-    });
-}
-handlePosts();
-function savePost(posts){
-  console.log(posts);
+  // Check answers
+  if (q1 == answers[0]) {
+    score++;
+  } else if (q2 == answers[1]) {
+    
+  }
 }
